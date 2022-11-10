892f31bdbebec9f103f0c5300e63bc8ac3d25005
M Afifah <mafifah.usr@gmail.com>
2022-11-10 10:00:37 +0700

    Add project files.

---
+++ DX/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DX;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ DX/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DX;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
