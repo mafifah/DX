bb3841dc0fdcdeb78d61ecfebdd48229dd52beea
devazamili <38879571+devatrivanus18@users.noreply.github.com>
2022-11-16 08:41:20 +0700

    edit profile dan index page

--- DX/ThemeItem.cs
+++ DX/ThemeItem.cs
@@ -12,8 +12,8 @@ namespace DX
         public static ThemeItem Create(string name)
         {
 
-            if (Utils.DevExpressThemes.Contains(name))
-                return new ThemeItem(name, new[] { $"_content/DevExpress.Blazor.Themes/{name}.bs5.min.css" });
+            //if (Utils.DevExpressThemes.Contains(name))
+               // return new ThemeItem(name, new[] { $"_content/DevExpress.Blazor.Themes/{name}.bs5.min.css" });
             return new ThemeItem(name, new[] { "_content/DevExpress.Blazor.Themes/bootstrap-external.bs5.min.css", $"css/switcher-resources/themes/{name}/bootstrap.min.css" });
         }
     };
